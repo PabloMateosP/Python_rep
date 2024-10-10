18160b2dc2317385a2278ce0315eccd9e6b21229
PabloMateosP <pablomateos1301@gmail.com>
2024-10-10 17:45:52 +0200

    Cuarto commit con pruebas - PMP - 10/10/24

--- pruebas/20.py
+++ pruebas/20.py
@@ -52,3 +52,10 @@ or	    Returns True if one of the statements is true	            x < 5 or x < 4
 not	    Reverse the result, returns False if the result is true	    not(x < 5 and x < 10)
 
 '''
+# ==========================
+'''
+Python Identity Operators
+is 	     Returns True if both variables are the same object	        x is y	
+is not	 Returns True if both variables are not the same object	    x is not y
+
+'''
