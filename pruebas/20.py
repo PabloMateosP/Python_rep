976e98cd67f404e8b369d4f4e9b86b3aa31e5c2f
PabloMateosP <pablomateos1301@gmail.com>
2024-10-11 12:52:09 +0200

    Quinto commit pruebas - PMP - 11/10/24

--- pruebas/20.py
+++ pruebas/20.py
@@ -59,3 +59,27 @@ is 	     Returns True if both variables are the same object	        x is y
 is not	 Returns True if both variables are not the same object	    x is not y
 
 '''
+# ==========================
+'''
+Python Merbership Operators 
+in 	    Returns True if a sequence with the specified value is present in the object	    x in y	
+not in	Returns True if a sequence with the specified value is not present in the object	x not in y
+
+'''
+# ==========================
+'''
+Python Bitwise Operators 
+& 	AND	Sets each bit to 1 if both bits are 1	            x & y	
+|	OR	Sets each bit to 1 if one of two bits is 1	        x | y	
+^	XOR	Sets each bit to 1 if only one of two bits is 1	    x ^ y	
+~	NOT	Inverts all the bits	                            ~x	
+<<	Zero fill left shift	Shift left by pushing zeros in from the right and let the leftmost bits fall off	                      x << 2	
+>>	Signed right shift	    Shift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off	  x >> 2
+
+'''
+# ==========================
+'''
+Operators Precedence 
+Like math operators there is a order of precedence
+parentheses, exponents, multiplication and division from left to right, and addition and subtraction from left to right
+'''
